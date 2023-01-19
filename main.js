b12acf4a0b75671419a5222b81fa1774c60a8b21
Wale_Yusuf <100405598+Algebra0001@users.noreply.github.com>
2023-01-19 10:44:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1125,3 +1125,24 @@ function transpose(matrix){
     return result
 }
 console.log(transpose([[1,2,3], [4,5,6]]))
+
+// A bianry gap within a positive integer N any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+// For example;
+// Number 9 has a binary representation of 1001 and contains a binary gap of length 2. The number 529bhas binary representation of 1000010001 and contains two binary gaps of length 4 and 3. The number 20 has a binary represenation of 10100 has a binary gap of length 1. The number 15 has a representation of 1111 and has no binary gaps. The number 32 has a representation of 100000 and has no binary gaps.
+// Your task: 
+// Write a function solution(N) that given a positive a integer N, returns the length of its longest binarygap. The function should return 0 if n N does not have any binary gap..
+function solution(N){
+    N = N.toString(2).split("");
+    let maxGap = 0;
+    let currentGap = 0;
+    for(let i = 0; i<N.length; i++){
+        if(N[i] === "1"){
+            maxGap = Math.max(maxGap, currentGap)
+            currentGap = 0
+        } else {
+            currentGap++
+        }
+    }
+    console.log(maxGap)
+}
+solution(9)
